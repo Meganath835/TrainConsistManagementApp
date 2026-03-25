2e7f9bcecec2e62dc81b26d3564b08b653489872
Meganath Saravanan <gmmeganath@gmail.com>
2026-03-25 09:25:08 +0530

    Base application setup

---
+++ src/TrainConsistManagementApp.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class TrainConsistManagementApp {
+    public static void main(String[] args){
+        System.out.println("Base Application Setup");
+
+    }
+}
